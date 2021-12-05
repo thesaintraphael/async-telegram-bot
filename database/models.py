d681be0b77d13698d66e391d2cc398b644c4b086
Rafael Salimov <raphasalimov@gmail.com>
2021-12-05 16:44:44 +0400

    solved db issue

--- database/models.py
+++ database/models.py
@@ -4,8 +4,9 @@ from tortoise import models, fields
 class User(models.Model):
 
     tg_id = fields.CharField(max_length=255)
-    name = fields.CharField(max_length=255)
-    date_subcribed = fields.DatetimeField(auto_now_add=True)
+    name = fields.CharField(max_length=255, null=True)
+    username = fields.CharField(max_length=255, null=True, blank=True)
+    date_subscribed = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.name
