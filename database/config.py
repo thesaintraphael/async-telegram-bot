44b3b591a282789f19a3516173d4974f28e06354
Rafael Salimov <raphasalimov@gmail.com>
2021-12-05 12:56:45 +0400

    set up aerich to manage migrations

--- database/config.py
+++ database/config.py
@@ -1,4 +1,18 @@
 from decouple import config
 
 
-DATABASE_URL = config("DATABASE_URL")
+DEBUG = config("DEBUG")
+if DEBUG:
+    DATABASE_URL = config("DATABASE_TEST_URL")
+else:
+    DATABASE_URL = config("DATABSE_URL")
+
+TORTOISE_ORM = {
+    "connections": {"default": DATABASE_URL},
+    "apps": {
+        "models": {
+            "models": ["database.models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
--- database/connect.py
+++ database/connect.py
@@ -1,11 +1,14 @@
 from tortoise import Tortoise, run_async
-# from .config import DATABASE_URL
+
 import config
 
 print(config.DATABASE_URL)
 
+
 async def init():
-    await Tortoise.init(db_url=config.DATABASE_URL, modules={"models": ["models"]})
+    await Tortoise.init(
+        db_url=config.DATABASE_URL, modules={"models": ["models", "aerich.models"]}
+    )
     await Tortoise.generate_schemas()
 
 
